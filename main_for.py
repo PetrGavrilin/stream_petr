d07f9b3a4f75ef684136954f73a53f59769edfd9
Petr <114101583+PetrGavrilin@users.noreply.github.com>
2023-04-23 10:10:26 +0300

    Update main_for.py

--- main_for.py
+++ main_for.py
@@ -6,7 +6,9 @@ def main():
     
     import lite_ap
 
-    api_for = vk.API(access_token=token)  # адрес токена вк
+    #api_for = vk.API(access_token=token)  # адрес токена вк
+    
+    api_for = None
 
     lite_ap.main_for_all(api_for)
     
